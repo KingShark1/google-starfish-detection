cda2cab722263ff537d68e207827892c572b16af
KingShark1 <manastiwari28@gmail.com>
2021-12-10 15:21:31 +0530

    set up folder structure

---
+++ manage.py
@@ -0,0 +1,4 @@
+# TODO : Setup enviorment using command line
+# TODO : Run experiment and log results in a text file
+# TODO : Save the experiment results in a output directory, with training, validation, loss,  graphs
+#        along with results on testing set along with some gif or jpeg
